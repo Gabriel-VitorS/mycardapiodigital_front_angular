07522a1143be000bd65c5ab7fccb9ff3bdbdd4d7
Gabriel <gabrielvitordasilva@gmail.com>
2026-03-04 11:34:46 -0300

    feat: - adicionado componet spinner sm

---
+++ src/app/shared/components/ui/spinner-sm/spinner-sm.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-spinner-sm',
+  templateUrl: './spinner-sm.component.html',
+  styleUrls: ['./spinner-sm.component.scss']
+})
+export class SpinnerSmComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
